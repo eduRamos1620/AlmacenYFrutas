80eeac7946918666a3f3c077e4629195a43aa71e
Eduardo Ramos <eduardoramosrosales16@gmail.com>
2024-07-01 11:13:00 -0600

    se inician variables, crea constructor y getter en Fruta

--- src/com/ramos/almacenyverduleria/Fruta.java
+++ src/com/ramos/almacenyverduleria/Fruta.java
@@ -1,4 +1,21 @@
 package com.ramos.almacenyverduleria;
 
-public class Fruta {
+public class Fruta extends Producto{
+    private double peso;
+    private String color;
+
+    public Fruta(String nombre, double precio, double peso, String color){
+        super(nombre, precio);
+        this.peso = peso;
+        this.color = color;
+    }
+
+    public double getpeso(){
+        return peso;
+    }
+
+    public String getColor(){
+        return color;
+    }
+
 }
